1a1c4fbe7b889560484155b4be38872c73fd0e16
Alexander Popkov <popkovaa@ariant.ru>
2022-03-27 22:16:49 +0500

    lesson 2

--- app/src/androidTest/java/ru/netology/netologyvoiceassistant/FirstLessonInstrumentedTest.kt
+++ app/src/androidTest/java/ru/netology/netologyvoiceassistant/FirstLessonInstrumentedTest.kt
@@ -1,23 +1,13 @@
 package ru.netology.netologyvoiceassistant
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
-import com.example.netologyvoiceassistant.R
-import org.hamcrest.CoreMatchers.`is`
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
+//import ru.netology.netologyvoiceassistant.R
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@LargeTest
+/*@LargeTest
 @RunWith(AndroidJUnit4::class)
 class FirstLessonInstrumentedTest {
 
@@ -46,4 +36,4 @@ class FirstLessonInstrumentedTest {
         )
     }
 
-}
+}*/
